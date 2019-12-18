343735b92db24ac4a899fe1692234b469c077010
Hardstuck <36950734+Hardstuck@users.noreply.github.com>
2019-12-18 10:34:03 +0100

    Add files via upload

--- main.r
+++ main.r
@@ -1,15 +1,33 @@
 data <- read.csv("trainingset.csv")
 library(nnet)
 library(caret)
-training <- data[sample.int(10000, 100, replace = F),]
-#learn <- sample(1:nrow(data), round(2*nrow(data)/3))
-#model <- nnet(letter~., data = data, size = 2, maxit = 5000, decay = 0)#(no fuckin idea what does letter~. means)
-#model <- nnet(letter~., data = data, subset=learn size = 2, maxit = 5000, decay = 0)#(no fuckin idea what does letter~. means)
+set.seed(5)
+
+#If you don't want to wait 4 hours do not run it!
+#training <- data[sample.int(10000, 100, replace = F),]
+#sizes <- seq(1,20,by=2)
+#trc <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+#model.10x10CV <- train(V36~., data = training, method = 'nnet', maxit = 500, trace = FALSE, tuneGrid = expand.grid(.size=sizes ,.decay = 0), trControl = trc)
+
+#resulted model in model.10x10CVF.regul
+
+size <- 14
+decay <- 0.5011872
+
+learn <- sample(1:nrow(data), round(2*nrow(data)/3))
+#model <- nnet(V36~., data = data, size = size, maxit = 5000, decay = decay)
+model.nnet <- nnet(V36~., data = data, subset=learn, size = 10, maxit = 5000, decay = decay)
 
 #p2 <- as.factor(predict(model.nnet, newdata=test[c(1:nrow(test),], type = "class"))
-#p2 <- as.factor(predict(model.nnet, newdata=data[-learn,], type = "class"))
+nlearn <- nrow(data[learn,])
+p1 <- as.factor(predict(model.nnet, type = "class"))
+t1 <- table(p1, data$V36[learn])
+error_rate.train <- 100*(1-sum(diag(t1))/nlearn)
 
-sizes <- seq(1,20,by=2)
-trc <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
-model.10x10CV <- train(V36~., data = training, method = 'nnet', maxit = 500, trace = FALSE, tuneGrid = expand.grid(.size=sizes ,.decay = 0), trControl = trc)
-model.10x10CV$results.
+ntest <- nrow(data[-learn,])
+p2 <- as.factor(predict(model.nnet, newdata=data[-learn,], type = "class"))
+t2 <- table(p2, data$V36[-learn])
+error_rate.test <- 100*(1-sum(diag(t2))/ntest)
+
+#error_rate.train 0.239988
+#error_rate.test 0.840084
